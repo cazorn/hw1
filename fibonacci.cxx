3431d597110cc8d2de606a4d4d9a7df70100032c
Cazorn <cazorn@linux-ghqt.site>
2015-10-28 11:41:35 +0100

    Abgabe von K. Paramasivam

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main(){
-  std::cout << "hallo" << std::endl;
+  using namespace std;
+  int N;
+  cout <<"Type number to calculate it's fib. number"<< endl;		//Input to cal. F(N)
+  cout <<"N="<< endl;							
+  cin >> N;								//save input in N
+  int f0=0,f1=1,fz;
+  for (int i=1; i<N; i++){						//do for loop till F(N) is calc. -> i goes from 1 to N
+    fz=f1;								//save the value of f1, need for the next loop
+    f1+=f0;								//f1=f1+f0
+    f0=fz;								//f0=fz -> f0 have got the former value of f1 needed to cal the next round
+  }
+  
+  cout <<"f(N)="<<f1<< endl;
   return 0;
 }
